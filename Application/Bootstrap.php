5490866eaacd9861d2b67417554d586b0fcecb53
Mathieu Desvé <mathieu.desve@me.com>
2012-04-15 20:34:33 +0000

    - Add Acces Control Header

--- Application/Bootstrap.php
+++ Application/Bootstrap.php
@@ -2,6 +2,13 @@
 
 	class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 	{
+		protected function _initAccessControl()
+		{
+			header("Access-Control-Allow-Origin: *");
+			header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+			header('Access-Control-Allow-Headers: X-Requested-With, Authorization, authorization');
+		}
+		
 		protected function _initAutoload()
 		{
 			$autoloader = Zend_Loader_Autoloader::getInstance();
